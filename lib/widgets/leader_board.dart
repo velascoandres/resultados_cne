f28be0f28d59f9ff73ee2bfe6d73f5f6afb461f0
andres <velasco.andrs@gmail.com>
2021-02-11 17:52:29 -0500

    leaderboard stack

--- lib/widgets/leader_board.dart
+++ lib/widgets/leader_board.dart
@@ -3,39 +3,59 @@ part of 'widgets.dart';
 class LeaderBoard extends StatelessWidget {
   final List<Dato> collection;
   final int deepLevel;
+
   int _totalVotes;
+  List<Dato> _orderedCollection;
 
   LeaderBoard({@required this.collection, this.deepLevel = 3}) {
-    this.collection.sort((a, b) => b.intVotos.compareTo(a.intVotos));
-    _totalVotes = this.collection.fold<int>(
+    _orderedCollection = [...this.collection];
+    _orderedCollection.sort((a, b) => b.intVotos.compareTo(a.intVotos));
+    _totalVotes = this._orderedCollection.fold<int>(
           0,
           (int acc, Dato dato) => acc + dato.intVotos,
-    );
+        );
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return AspectRatio(
       aspectRatio: 2,
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        child: ListView.builder(
-          itemCount: deepLevel,
-          itemBuilder: _buildLeaderItem,
+      child: Stack(children: [
+        Container(
+          padding: EdgeInsets.only(top: 20),
+          child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: ListView.builder(
+              itemCount: deepLevel,
+              itemBuilder: _buildLeaderItem,
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          top: 1,
+          left: MediaQuery.of(context).size.width / 3,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.redAccent,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text('Top $deepLevel de candidatos'),
+          ),
+        ),
+      ]),
     );
   }
 
   Widget _buildLeaderItem(BuildContext _, int positionIndex) {
-          final candidate = this.collection[positionIndex];
-  
-          final percent =  (candidate.intVotos * 100 / _totalVotes).toStringAsFixed(2);
-  
-          return ListTile(
-            leading: Text('${candidate.strNomCandidato}'),
-            trailing: Text('$percent %'),
-          );
-        }
+    final candidate = this._orderedCollection[positionIndex];
+
+    final percent = (candidate.intVotos * 100 / _totalVotes).toStringAsFixed(2);
+
+    return ListTile(
+      leading: Text('${candidate.strNomCandidato}'),
+      trailing: Text('$percent %'),
+    );
+  }
 }
